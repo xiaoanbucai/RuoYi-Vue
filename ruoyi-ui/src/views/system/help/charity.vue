5b98342fb1b7b2d7cba127d67561c98308bec772
xiaoanbucai <46344743+xiaoanbucai@users.noreply.github.com>
2021-01-28 17:14:37 +0800

    2021.01.28

--- ruoyi-ui/src/views/system/help/charity.vue
+++ ruoyi-ui/src/views/system/help/charity.vue
@@ -1,37 +1,126 @@
 //家庭慈善救助页面 <
 <template>
   <div class="app-container">
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span style="text-align: center">家庭慈善救助办理常见问题以及办理指导</span>
-      </div>
-      <el-upload
-        style="text-align: center"
-        class="upload-demo"
-        accept=".doc, .docx"
-        drag
-        multiple
-        :file-list="fileList"
-        :auto-upload="false"
-      >
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      </el-upload>
-      <el-button
+    <el-col :span="8">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span style="text-align: center">家庭慈善救助办理常见问题以及办理指导</span>
+        </div>
+        <el-collapse v-model="activeNames" @change="handleChange">
+          <el-collapse-item title="家庭慈善救助原则" name="1">
+            <div>1.坚持“公开、公平、公正”的原则，接受社会各界监督；</div>
+            <div>
+              2.坚持“生活最困难、遭遇最不幸、社会最同情”的原则核定受助对象，救到急处、实处、难处；
+            </div>
+            <div>
+              3.坚持“助困不助懒、扶贫更扶志”的原则，鼓励受助对象勤劳自救、积极进取，早日脱离困境；
+            </div>
+            <div>4.坚持以“慈善项目”为依托，针对符合项目条件的受助对象实施救助；</div>
+            <div>
+              5.坚持“不重复救助”的原则，对受助对象原则上每年度只救助一次，三年不超过两次救助，五年不超过三次救助。
+            </div>
+          </el-collapse-item>
+          <el-collapse-item title="家庭慈善救助范围及对象" name="2">
+            <div>
+              1.具有郑州市户口，因患重大疾病或天灾人祸等突发事件导致生活困难者；因孤、老、残导致生活困难者。
+            </div>
+            <div>2.符合本会当年发布的慈善项目中的救助对象。</div>
+            <div>3.根据捐赠者的意愿所实施的救助。</div>
+            <div>4.根据上级要求，实施的省内外赈灾和援助项目。</div>
+          </el-collapse-item>
+          <el-collapse-item title="申请程序" name="3">
+            <div>
+              1.如符合当年郑州市慈善项目救助范围的，可以直接咨询项目合作单位、或直接到项目合作单位进行求助。
+            </div>
+            <div>
+              2.如符合郑州慈善总会救急难项目救助范围的，可直接联系各县（市、区）、开发区慈善总会申请救助。
+            </div>
+            <div>
+              3.如不符合当年郑州市慈善项目救助范围的，按照户籍属地管理的原则和谁募集谁使用的工作方针，各县（市）、区困难群众的临时救助由所在地慈善总会负责。
+            </div>
+            <div>
+              4.向郑州慈善总会直接捐赠资金的单位干部、职工符合救助条件的，可由捐赠单位提出申请（提交材料与个人申请救助的材料相同），并填写《郑州慈善总会救助申请表》。
+            </div>
+            <div>
+              5.捐赠者的定向救助，由定向捐赠协议中规定的救助对象提出申请及相关证明材料，并经捐赠方审核同意后，根据《郑州慈善总会章程》实施救助。
+            </div>
+          </el-collapse-item>
+          <el-collapse-item title="所需提交材料:" name="5">
+            <div>附申请人户口本和身份证复印件。</div>
+            <div>属低保或低收入家庭的，需提供低保证或低收入家庭证明复印件；</div>
+            <div>属残疾人的，需提供残疾人证复印件；</div>
+            <div>
+              属因病致贫的，应提供医疗单位的诊断证明、医疗费收据复印件，户口所在地县（区）、乡（街道办）、村（居）民委员会证明（需注明家庭成员基本信息）。
+            </div>
+          </el-collapse-item>
+          <el-collapse-item title="各县（市）区慈善总会咨询电话:" name="6">
+            <div>郑州慈善总会：68665328</div>
+            <div>二七区：68769566</div>
+            <div>管城区：66378793</div>
+            <div>金水区：58579013</div>
+            <div>中原区：67693811</div>
+            <div>上街区：85701691</div>
+            <div>经开区：66781811</div>
+            <div>惠济区：63639736</div>
+            <div>荥阳市：64650881</div>
+            <div>新密市：69822497</div>
+            <div>新郑市：69902200</div>
+            <div>登封市：85506662</div>
+            <div>中牟市：62188811</div>
+            <div>高新区：67987922</div>
+            <div>郑东新区：67179202</div>
+            <div>航空港区：86199766</div>
+          </el-collapse-item>
+        </el-collapse>
+      </el-card>
+    </el-col>
+    <el-col :span="8">
+      <el-card class="box-card2">
+        <div slot="header" class="clearfix">
+          <span>业务办理相关材料提交</span>
+        </div>
+        <el-collapse v-model="activeNames" @change="handleChange"> </el-collapse>
+        <el-upload
+          style="text-align: center; margin-top: 20px"
+          class="upload-demo"
+          accept=".doc, .docx"
+          action=""
+          drag
+          multiple
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :file-list="fileList"
+          :on-exceed="handleExceed"
+          :auto-upload="false"
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">
+            <el-checkbox/>是否更新已经存在的用户数据
+            <el-link type="info" style="font-size: 12px" @click=""
+              >下载模板</el-link
+            >
+          </div>
+        </el-upload>
+
+        <!-- <el-button
         style="margin: 15px 0px 0px 35%"
         size="small"
         type="success"
         @click="submitUpload"
         >上传到服务器</el-button
-      >
-    </el-card>
+      > -->
+      </el-card>
+    </el-col>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      activeNames: ["1"],
+    };
   },
   methods: {
     submitUpload() {
@@ -42,6 +131,16 @@ export default {
     },
     handlePreview(file) {
       console.log(file);
+    },
+    handleChange(val) {
+      console.log(val);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${
+          files.length + fileList.length
+        } 个文件`
+      );
     },
   },
 };
@@ -66,5 +165,10 @@ export default {
 
 .box-card {
   width: 480px;
+}
+
+.box-card2 {
+  width: 480px;
+  margin-left: 240px;
 }
 </style>
--- ruoyi-ui/src/views/system/help/orphan.vue
+++ ruoyi-ui/src/views/system/help/orphan.vue
@@ -1,22 +1,80 @@
 //孤儿救助金办理页面
 <template>
   <div class="app-container">
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>孤儿救助金办理常见问题以及办理指导</span>
-        <!-- <el-button style="float: right; padding: 3px 0" type="text">上传文件</el-button> -->
-      </div>
-      <el-collapse v-model="activeNames" @change="handleChange">
-        <el-collapse-item title="郑州低保标准" name="1">
-          <div>城镇低保标准</div>
-          <div>
-            从2015年7月1日起，郑州市城市低保标准将由现在每人每月470元提高至520元。
-          </div>
-          <div>农村低保标准</div>
-          <div>从2015年7月1日起，郑州市农村低保标准由每人每月260元提高至290元。</div>
-        </el-collapse-item>
-      </el-collapse>
-    </el-card>
+    <el-col :span="8">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>孤儿救助金办理常见问题以及办理指导</span>
+          <!-- <el-button style="float: right; padding: 3px 0" type="text">上传文件</el-button> -->
+        </div>
+        <el-collapse v-model="activeNames" @change="handleChange">
+          <el-collapse-item title="散居孤儿申请孤儿基本生活费条件以及所需材料" name="1">
+            <div>
+              由孤儿监护人（抚养人）向孤儿户籍所在地街道办事处或乡（镇）人民政府提出申请。
+            </div>
+            <div>申请时应提交以下材料：</div>
+            <div>
+              1.公安机关出具的孤儿父母死亡户口本销户页或证明，或医院出具的孤儿父母死亡证明，或殡仪馆出具的孤儿父母死亡火化证明，或人民法院出具的孤儿父母宣告死亡或失踪的证明法律文书原件;
+            </div>
+            <div>2.孤儿身份证（或户口本）原件；</div>
+            <div>3.孤儿监护人（抚养人）身份证（或户口本）原件；</div>
+            <div>4.孤儿近期1寸免冠照片2张；</div>
+            <div>5.孤儿监护人（抚养人）的贫困证明；</div>
+            <div>6.残疾孤儿需提供残疾证明。</div>
+          </el-collapse-item>
+          <el-collapse-item title="散居孤儿申请孤儿基本生活费审核流程" name="2">
+            <div>
+              街道办事处或乡（镇）人民政府应在10个工作日内，组织有关人员通过入户调查、邻里访问等方式，对监护人（抚养人）和社会散居孤儿情况进行认真核实。
+            </div>
+            <div>
+              符合认定条件的，由孤儿本人或监护人（抚养人）填写《孤儿基本生活费申报审批表》一式两份，街道办事处或乡（镇）人民政府签署审查意见后，连同有关证明材料的复印件一并报县级民政部门审批。
+            </div>
+          </el-collapse-item>
+          <el-collapse-item title="散居孤儿申请孤儿基本生活费审批流程" name="3">
+            <div>
+              县级民政部门要认真审核申请材料，符合条件的，应在10个工作日内作出审批决定，并将审批结果函告街道办事处或乡（镇）人民政府。
+            </div>
+            <div>
+              县级民政部门应同时与社会散居孤儿的监护人（抚养人）签定孤儿养育协议，对领取、使用孤儿基本生活费以及孤儿养育状况提出相应要求，明确监护人（抚养人）应依法履行的监护职责和抚养义务。
+            </div>
+            <div>签订孤儿养育协议应征求和尊重孤儿本人意愿。</div>
+            <div>孤儿监护人（抚养人）发生变化的，应按照以上程序重新办理有关手续。</div>
+          </el-collapse-item>
+          <el-collapse-item title="如何办理孤儿证：" name="4">
+            <div>孤儿是指失去父母的儿童。</div>
+            <div>
+              凡具有本地户口、年龄未满十八周岁,失去父母,经户口所在地县民政部门审核,报市民政局认定批准后发给《孤儿证》的儿童。
+            </div>
+          </el-collapse-item>
+          <el-collapse-item title="办理孤儿证申请材料：" name="5">
+            <div>1、户口簿(父母死亡户籍证明)；</div>
+            <div>2、生父母死亡或者宣告死亡的证明。</div>
+            <div>
+              孤儿的父母因病自然死亡的，凡死于医疗机构的，死亡证明由该医疗机构出具；死于家中的，死亡证明由负责该地区的基层医疗卫生组织出具；
+            </div>
+            <div>
+              凡非正常死亡或不能确定是否属于正常死亡者，需经公安部门判定性质并出具死亡证明；宣告死亡的证明则应由人民法院出具。
+            </div>
+          </el-collapse-item>
+          <el-collapse-item title="办理孤儿证办理程序：" name="6">
+            <div>1.由本人提出书面申请并附户口簿复印件</div>
+            <div>2.村委会调查落实</div>
+            <div>3.乡民政办核实并填报审批表</div>
+            <div>4.交分管领导签字审批</div>
+            <div>5.报县民政局审批</div>
+            <div>6.由县民政局填报孤儿证</div>
+            <div>7.转乡民政办审批并通知申请人领取孤儿证</div>
+          </el-collapse-item>
+        </el-collapse>
+      </el-card>
+    </el-col>
+    <el-col :span="8">
+      <el-card class="box-card2">
+        <div slot="header" class="clearfix">
+          <span>业务办理相关材料提交</span>
+        </div>
+      </el-card>
+    </el-col>
   </div>
 </template>
 
@@ -55,5 +113,10 @@ export default {
 
 .box-card {
   width: 480px;
+}
+
+.box-card2 {
+  width: 480px;
+  margin-left: 240px;
 }
 </style>
